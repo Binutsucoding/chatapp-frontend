711b0beb83c22118a51db82ded5c934c80ce4d9b
unknown <utsavshah972@gmail>
2024-03-26 18:03:12 +0530

    done

---
+++ src/constants/color.js
@@ -0,0 +1,15 @@
+export const orange = "#ea7070";
+export const orangeLight = "rgba(234, 112, 112,0.2)";
+
+export const grayColor = "rgba(247,247,247,1)";
+export const lightBlue = "#2694ab";
+export const matBlack = "#1c1c1c";
+
+export const bgGradient = "linear-gradient(rgb(255 225 209), rgb(249 159 159))";
+export const bgGradient1 =
+  "linear-gradient(rgba(200,200,300,0.5),rgba(1430,230,220,0.5))";
+export const bgGradient2 =
+  "linear-gradient(rgba(200,200,200,0.4),rgba(1230,230,220,0.91))";
+
+export const purple = "rgba(75,12,192,1)";
+export const purpleLight = "rgba(75,12,192,0.2)";
